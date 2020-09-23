5dc1876ad78c48c24c6a599d78da7df77062a4ae
Victor Irekponor <marquisvictor93@gmail.com>
2020-09-23 13:44:23 +0100

    learnt how to show props in browser rather than developer tools

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import { Router } from '@reach/router';
 import Pet from "./Pet";
+import Details from "./Details";
 
 const App = () => {
 	return (
 		<React.StrictMode>
 			<div>
 				<h1></h1>
-				<SearchParams />
+
+				<Router>
+					<SearchParams path="/" />
+					<Details path="/details/:1" />
+				</Router>
+				
 			</div>
 		</React.StrictMode>
 
