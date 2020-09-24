19cb756a3671a2a8b7e12da93b25b9aebd7e0460
Victor Irekponor <marquisvictor93@gmail.com>
2020-09-24 02:11:42 +0100

    introduction to react context API :construction:

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
 import ErrorBoundary from './ErrorBoundary';
+import ThemeContext from './ThemeContext';
 
 class Details extends React.Component {
     state = {
@@ -10,7 +11,7 @@ class Details extends React.Component {
 
     componentDidMount() {
 
-        throw new Error("lol");
+        // throw new Error("lol");
 
         Pet.animal(this.props.id).then(({ animal }) => {
             this.setState({
@@ -37,7 +38,12 @@ class Details extends React.Component {
                 <div>
                     <h1>{name}</h1>
                     <h2> { `${animal} -${breed} - ${location}` } </h2>
-                    <button>Adopt {name} </button>
+
+                    <ThemeContext.Consumer>
+                        {(themeHook) => (
+                            <button style={{backgroundColor: themeHook[0]}}>Adopt {name} </button>
+                        )}
+                    </ThemeContext.Consumer>
                     <p> {description} </p>
                 </div>
             </div>
---
+++ src/ThemeContext.js
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+
+
+const ThemeContext = createContext(["green", ()=> {}]);
+
+
+
+export default ThemeContext;
